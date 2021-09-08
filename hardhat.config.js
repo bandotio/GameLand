ca9da2841cac4c6c918550d4a6a458a92957de4c
bandotadmin <contact@bandot.io>
2021-09-08 15:31:45 +0800

    add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+require('dotenv').config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -10,12 +16,35 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet_aurora: {
+      url: 'https://testnet.aurora.dev',
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 1313161555,
+      gasPrice: 120 * 1000000000
+    },
+    local_aurora: {
+      url: 'http://localhost:8545',
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 1313161555,
+      gasPrice: 120 * 1000000000
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 3,
+      live: true,
+      gasPrice: 50000000000,
+      gasMultiplier: 2,
+    },
+  }
 };
